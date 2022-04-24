8f67596c84e9b9281c3b5cf450bbb7f452645147
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-04-24 14:08:54 +0700

    OneToMany

---
+++ src/com/luv2code/test/onetomany/DeleteCourseDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.test.onetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.entity.onetomany.Course;
+import com.luv2code.entity.onetomany.Instructor;
+import com.luv2code.entity.onetomany.InstructorDetail;
+
+public class DeleteCourseDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+													.addAnnotatedClass(Instructor.class)
+													.addAnnotatedClass(InstructorDetail.class)
+													.addAnnotatedClass(Course.class)
+													.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get a course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
+
+			// delete course
+			System.out.println("Deleting course: " + tempCourse);
+
+			session.delete(tempCourse);
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+
+			// add clean up code
+			session.close();
+
+			factory.close();
+		}
+	}
+
+}
