775ca04cb3c0879173f182dab7eb259378b0ab77
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-04-25 22:54:10 +0700

    OneToManyUni

---
+++ src/com/luv2code/test/onetomanyuni/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,62 @@
+package com.luv2code.test.onetomanyuni;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.entity.onetomanyuni.Course;
+import com.luv2code.entity.onetomanyuni.Instructor;
+import com.luv2code.entity.onetomanyuni.InstructorDetail;
+import com.luv2code.entity.onetomanyuni.Review;
+
+
+public class DeleteCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+
+			// get the course
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
+			
+			// print the course
+			System.out.println("Deleting the course ... ");
+			System.out.println(tempCourse);
+			
+			// print the course reviews
+			System.out.println(tempCourse.getReviews());
+			
+			// delete the course
+			session.delete(tempCourse);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
